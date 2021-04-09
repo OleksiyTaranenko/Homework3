dcf99c6370295953c00d5421896d273ff8f79417
Oleksiy_Taranenko <oleksiytaranenko01@gmail.com>
2021-04-09 10:42:02 +0300

    homework3

---
+++ js/script2.js
@@ -0,0 +1,27 @@
+//And please uncomment this page :)
+
+
+// let unit = 303.15;
+
+// let f = 86;
+// let c = 30;
+// let k = 303.15;
+
+// let notice = 'Real temperature is:';
+
+// if (unit <= f) {
+//     console.log(notice);
+//     console.log('Fahrenheit:', f);
+//     console.log('Celsius:', (f - 32) * 5/9);
+//     console.log('Kelvin:', c + 273.15);
+// } else if (unit <= c) {
+//     console.log(notice);
+//     console.log('Celsius:', c);
+//     console.log('Fahrenheit:', (c * 9/5) + 32);
+//     console.log('Kelvin:', c + 273.15);        
+// } else {
+//     console.log(notice);
+//     console.log('Kelvin:', k);
+//     console.log('Celsius:', k - 273.15);
+//     console.log('Fahrenheit:', (c * 9/5) + 32);    
+// }
